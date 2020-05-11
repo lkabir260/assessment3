17d3099c26c9b520bc8507c5e36fb1cd89d211ef
Lamia Kabir <lkabir260@gmail.com>
2020-05-10 22:59:51 -0400

    Updates

---
+++ precache-manifest.e67b68fafd10995f0fd2f1721f8e45b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf6a3168e1455581454aec8fc198c55",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "2995eaf7ad50ed7f47b5",
+    "url": "/project-4-assessment/static/css/main.4dfdc0bf.chunk.css"
+  },
+  {
+    "revision": "2cbd2fa35beae51aa081",
+    "url": "/project-4-assessment/static/js/2.67407344.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.67407344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2995eaf7ad50ed7f47b5",
+    "url": "/project-4-assessment/static/js/main.88f9b98d.chunk.js"
+  },
+  {
+    "revision": "32238008bf55c999dcb1",
+    "url": "/project-4-assessment/static/js/runtime-main.0254daa0.js"
+  }
+]);
